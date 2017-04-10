cd33a560b215d1242e544db47a194489705d060f
Will Searle <will@livehybrid.com>
2017-04-10 08:11:22 +0000

    Added refresh timer

--- go.php
+++ go.php
@@ -1,12 +1,16 @@
 <?php
 
 $url=$_GET['url'];
+unset($_GET['url']);
+foreach ($_GET as $key=>$val) {
+	$url .= "&".$key."=".$val;
+}
 
 
 ?>
 <html>
 <head>
-<META http-equiv="refresh" content="2;URL=<?php echo $url; ?>">
+<META http-equiv="refresh" content="0;URL=<?php echo $url; ?>">
 </head>
 <body>
 <center> You Are being redirected to <?php echo $url; ?> </center>
